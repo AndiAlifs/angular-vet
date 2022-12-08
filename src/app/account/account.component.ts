a222fc7b63f45b0f52c0728aa03d0a47d02af0bd
AndiAlifs <andyalyfsyah4@gmail.com>
2022-12-08 15:11:53 +0700

    account detail

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { Account } from '../account';
+import { AccountDetailComponent } from '../account-detail/account-detail.component';
 
 @Component({
   selector: 'app-account',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
+  accounts: Account[] = [{
+    nama: 'John Doe',
+    saldo: 1000000,
+    norek: '1234567890'
+  }, {
+    nama: 'Jane Doe',
+    saldo: 2000000,
+    norek: '0987654321'}]
+  ;
 
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  selectedAccount?: Account;
+
+  onSelect(account: Account): void {
+    this.selectedAccount = account;
+    console.log(this.selectedAccount);
+  }
 }
